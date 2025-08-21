4df113b0f1504487597f1d454c1c23812fa1e0d2
drew3670 <drewrichardson@mmc.school.nz>
2025-08-22 11:26:19 +1200

    Delete price_comparison_v1.py

--- price_comparison_v1.py
+++
@@ -1,9 +0,0 @@
-def make_statement(statement, decoration):
-    """Emphasises headings by adding decoration
-    at the start and end"""
-    return f"{decoration * 3} {statement} {decoration * 3}"
-
-
-# Program main heading
-print(make_statement("Welcome to Planty Plants ", "🌸"))
-print()
